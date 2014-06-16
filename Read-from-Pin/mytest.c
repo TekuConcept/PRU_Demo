d0c64640c0114ffa44fb1b6d162603cb173c64de
TekuConcept <TekuConcept@ymail.com>
2014-06-16 17:47:27 -0600

    Update mytest.c
    Tiding up C code

--- Read-from-Pin/mytest.c
+++ Read-from-Pin/mytest.c
@@ -1,6 +1,18 @@
 /*
- * Original Source from Shabaz (www.element14.com/community)
- * mytest.c
+ * ============================================================================
+ * Copyright (c) Texas Instruments Inc 2010-12
+ * Modified by TekuConcept 2014-6-16
+ *
+ * Use of this software is controlled by the terms and conditions found in the
+ * license agreement under which this software has been supplied or provided.
+ * ============================================================================
+ *
+ *
+ *
+ * This program demonstraits how one can read from a pin. The program will wait
+ * until pin P9.28 is HIGH. Then when it is HIGH, pin P9.27 is turned off and
+ * the program is completed.
+ *
  */
 
 /******************************************************************************
@@ -17,57 +29,17 @@
 
 // Driver header file
 #include "prussdrv.h"
-#include <pruss_intc_mapping.h>	 
-
-/******************************************************************************
-* Explicit External Declarations                                              *
-******************************************************************************/
+#include <pruss_intc_mapping.h>
 
 /******************************************************************************
 * Local Macro Declarations                                                    *
 ******************************************************************************/
 
-#define PRU_NUM 	 0
-#define ADDEND1	 	 0x98765400u
-#define ADDEND2		 0x12345678u
-#define ADDEND3		 0x10210210u
-
-#define DDR_BASEADDR     0x80000000
-#define OFFSET_DDR	 0x00001000 
-#define OFFSET_SHAREDRAM 2048		//equivalent with 0x00002000
-
-#define PRUSS0_SHARED_DATARAM    4
-
-/******************************************************************************
-* Local Typedef Declarations                                                  *
-******************************************************************************/
-
-
-/******************************************************************************
-* Local Function Declarations                                                 *
-******************************************************************************/
-
-static int LOCAL_exampleInit ( );
-static unsigned short LOCAL_examplePassed ( unsigned short pruNum );
-
-/******************************************************************************
-* Local Variable Definitions                                                  *
-******************************************************************************/
-
-
-/******************************************************************************
-* Intertupt Service Routines                                                  *
-******************************************************************************/
-
-
-/******************************************************************************
-* Global Variable Definitions                                                 *
-******************************************************************************/
-
-static int mem_fd;
-static void *ddrMem, *sharedMem;
-
-static unsigned int *sharedMem_int;
+#define PRU_NUM               0
+#define DDR_BASEADDR          0x80000000
+#define OFFSET_DDR            0x00001000 
+#define OFFSET_SHAREDRAM      2048		//equivalent with 0x00002000
+#define PRUSS0_SHARED_DATARAM 4
 
 /******************************************************************************
 * Global Function Definitions                                                 *
@@ -92,10 +64,6 @@ int main (void)
     
     /* Get the interrupt initialized */
     prussdrv_pruintc_init(&pruss_intc_initdata);
-
-    /* Initialize example */
-    printf("\tINFO: Initializing example.\r\n");
-    LOCAL_exampleInit(PRU_NUM);
     
     /* Execute example on PRU */
     printf("\tINFO: Executing example.\r\n");
@@ -107,72 +75,11 @@ int main (void)
     printf("\tINFO: PRU completed transfer.\r\n");
     prussdrv_pru_clear_event (PRU_EVTOUT_0, PRU0_ARM_INTERRUPT);
 
-    /* Check if example passed */
-    if ( LOCAL_examplePassed(PRU_NUM) )
-    {
-        printf("Example executed succesfully.\r\n");
-    }
-    else
-    {
-        printf("Example failed.\r\n");
-    }
+    // test if program passed, final variables are good, and clean up
     
     /* Disable PRU and close memory mapping*/
     prussdrv_pru_disable(PRU_NUM); 
     prussdrv_exit ();
-    munmap(ddrMem, 0x0FFFFFFF);
-    close(mem_fd);
-
-    return(0);
-}
-
-/*****************************************************************************
-* Local Function Definitions                                                 *
-*****************************************************************************/
-
-static int LOCAL_exampleInit (  )
-{
-    void *DDR_regaddr1, *DDR_regaddr2, *DDR_regaddr3;	
-
-    /* open the device */
-    mem_fd = open("/dev/mem", O_RDWR);
-    if (mem_fd < 0) {
-        printf("Failed to open /dev/mem (%s)\n", strerror(errno));
-        return -1;
-    }	
-
-    /* map the DDR memory */
-    ddrMem = mmap(0, 0x0FFFFFFF, PROT_WRITE | PROT_READ, MAP_SHARED, mem_fd, DDR_BASEADDR);
-    if (ddrMem == NULL) {
-        printf("Failed to map the device (%s)\n", strerror(errno));
-        close(mem_fd);
-        return -1;
-    }
     
-    /* Store Addends in DDR memory location */
-    DDR_regaddr1 = ddrMem + OFFSET_DDR;
-    DDR_regaddr2 = ddrMem + OFFSET_DDR + 0x00000004;
-    DDR_regaddr3 = ddrMem + OFFSET_DDR + 0x00000008;
-
-    *(unsigned long*) DDR_regaddr1 = ADDEND1;
-    *(unsigned long*) DDR_regaddr2 = ADDEND2;
-    *(unsigned long*) DDR_regaddr3 = ADDEND3;
-
     return(0);
-}
-
-static unsigned short LOCAL_examplePassed ( unsigned short pruNum )
-{
-    unsigned int result_0, result_1, result_2;
-
-     /* Allocate Shared PRU memory. */
-    prussdrv_map_prumem(PRUSS0_SHARED_DATARAM, &sharedMem);
-    sharedMem_int = (unsigned int*) sharedMem;
-
-    result_0 = sharedMem_int[OFFSET_SHAREDRAM];
-    result_1 = sharedMem_int[OFFSET_SHAREDRAM + 1];
-    result_2 = sharedMem_int[OFFSET_SHAREDRAM + 2];
-
-    return ((result_0 == ADDEND1) & (result_1 ==  ADDEND2) & (result_2 ==  ADDEND3)) ;
-
 }
