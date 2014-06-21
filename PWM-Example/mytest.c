b5bfc974a0a442f847666e08eff91d3240d9be77
TekuConcept <TekuConcept@ymail.com>
2014-06-21 17:36:59 -0600

    Create mytest.c

---
+++ PWM-Example/mytest.c
@@ -0,0 +1,90 @@
+/*
+ * ============================================================================
+ * Copyright (c) Texas Instruments Inc 2010-12
+ * Modified by TekuConcept 2014-6-16
+ *
+ * Use of this software is controlled by the terms and conditions found in the
+ * license agreement under which this software has been supplied or  provided.
+ * ============================================================================
+ *
+ *
+ * 
+ * This example demonstraits how to access the pulse-width modulation subsystem
+ * feature through the PRU without going through the operating system  via  the
+ * host.
+ * 
+ */
+
+/******************************************************************************
+* Include Files                                                               *
+******************************************************************************/
+
+// Standard header files
+#include <stdio.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+
+// Driver header file
+#include "prussdrv.h"
+#include <pruss_intc_mapping.h>
+
+/******************************************************************************
+* Local Macro Declarations                                                    *
+******************************************************************************/
+
+#define PRU_NUM 	 0
+
+#define DDR_BASEADDR 0x80000000
+#define OFFSET_DDR   0x00001000
+#define OFFSET_SHAREDRAM   2048 //equivalent with 0x00002000
+
+#define PRUSS0_SHARED_DATARAM 4
+
+/******************************************************************************
+* Global Function Definitions                                                 *
+******************************************************************************/
+
+int main (void)
+{
+    unsigned int ret;
+    tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
+
+    printf("\nINFO: Starting %s example.\r\n", "PRU_memAccess_DDR_PRUsharedRAM");
+    /* Initialize the PRU */
+    prussdrv_init ();
+
+    /* Open PRU Interrupt */
+    ret = prussdrv_open(PRU_EVTOUT_0);
+    if (ret)
+    {
+        printf("prussdrv_open open failed\n");
+        return (ret);
+    }
+
+    /* Get the interrupt initialized */
+    prussdrv_pruintc_init(&pruss_intc_initdata);
+
+    /* Initialize example */
+    printf("\tINFO: Initializing example.\r\n");
+
+    /* Execute example on PRU */
+    printf("\tINFO: Executing example.\r\n");
+    prussdrv_exec_program (PRU_NUM, "./prucode.bin");
+
+    /* Wait until PRU0 has finished execution */
+    printf("\tINFO: Waiting for HALT command.\r\n");
+    prussdrv_pru_wait_event (PRU_EVTOUT_0);
+    printf("\tINFO: PRU completed transfer.\r\n");
+    prussdrv_pru_clear_event (PRU_EVTOUT_0, PRU0_ARM_INTERRUPT);
+
+    printf("Example executed succesfully.\r\n");
+
+    /* Disable PRU and close memory mapping*/
+    prussdrv_pru_disable(PRU_NUM); 
+    prussdrv_exit ();
+
+    return(0);
+}
